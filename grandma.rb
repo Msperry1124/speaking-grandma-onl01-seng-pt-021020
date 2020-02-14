5d4a8f5ef0aee29fab704762cb3490e718daf72b
Melissa Sperry <sperry.mel@gmail.com>
2020-02-14 04:00:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
-else
+elsif(phrase)
   'HUH?! SPEAK UP, SONNY!'
+else
+  'NO, NOT SINCE 1928!'
 end
 end
